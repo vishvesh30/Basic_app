df86c31f18150e8fe6b02764aa612d4db9a44282
vishvraj <kapadia.vishvesh@gmail.com>
2017-01-11 13:04:28 +0530

    this is 2nd app with color 1

--- app/src/main/java/com/example/vishvraj/basic_app/MainActivity.java
+++ app/src/main/java/com/example/vishvraj/basic_app/MainActivity.java
@@ -1,19 +1,66 @@
 package com.example.vishvraj.basic_app;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private String spinnerdata[];
+    private static final int SELECT_PICTURE = 0;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerdata=new String[]{
+                "Ahmedabad","Surat","Gandhinagar","Vadodara","Rajkot"
+    };
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Spinner city=(Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adpter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,spinnerdata);
+        city.setAdapter(adpter);
+}
+    private Bitmap getPath(Uri uri) {
+
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String filePath = cursor.getString(column_index);
+        // cursor.close();
+        // Convert file path into bitmap image using below line.
+        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+
+        return bitmap;
     }
-    public void next(View v){
-        Intent intent=new Intent("com.example.vishvraj.basic_app.Main2Activity");
-        startActivity(intent);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bitmap bitmap = getPath(data.getData());
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    public void selectImage(View v) {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
     }
 }
